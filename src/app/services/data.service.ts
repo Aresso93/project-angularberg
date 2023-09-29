cae3ddf39c8ee8f83a37a2a602dc7031231a78bc
Aresso93 <ares.fiumicelli@gmail.com>
2023-09-29 09:55:40 +0200

    modifiche

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../model/book';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  allBooks: Book[] = []
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks(): void{
+    this.http.get<any>('https://gutendex.com/books/').pipe(
+      tap(data => console.log(data)),
+      map(data => data.results),
+      tap(books => console.log(books)))
+      // .subscribe(characters => this.allCharacters.next(characters));
+  }
+
+
 }
